9d455345265d159bef454c05321d3a3b4b382412
Vsevolod <seva@MacBook-Air-Vsevolod.local>
2026-09-11 09:45:00 +0300

    задание 4

--- task1laba2.js
+++ task1laba2.js
@@ -6,32 +6,6 @@ const grades = [
     { name: "Студент_X", score: 45 },
 ];
 
-function calculateAverage (data) {
-    const total = data.reduce((geo, people) => geo + people.score, 0)
-
-    return total / data.length;
-
-}
-
-console.log(calculateAverage(grades))
-
-function findTopStudent(data) {
-    const sc = data.map(student => student.score);
-    const maxscore = Math.max( ...sc)
-    const topstudent = data.find (student => student.score === maxscore)
-    return topstudent.name
-}
-console.log(findTopStudent(grades))
-
-function filterFailed(data, passScore) {
-return data
-
-    .filter (people => people.score >= passScore)
-    .map(people => people.name);
-}
-
-console.log(filterFailed(grades, 60))
-
 function addLetterGrade(data) {
 
     for (let i = 0; i < data.length; i++) {
