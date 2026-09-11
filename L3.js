f062270fedbb624cc2ad913fd7b58bc40c41e75c
Galen <dvoryanov2006@mail.ru>
2026-09-11 10:03:25 +0300

    test commit

---
+++ L3.js
@@ -0,0 +1,17 @@
+const grades = [
+    { name: "Макар", score: 85 },
+    { name: "Денис", score: 92 },
+    { name: "Анна", score: 78 },
+    { name: "Даша", score: 88 },
+    { name: "Студент_X", score: 45 },
+];
+
+
+function findTopStudent(data) {
+    const sc = data.map(student => student.score);
+    const maxscore = Math.max( ...sc)
+    const topstudent = data.find (student => student.score === maxscore)
+    return topstudent.name
+}
+console.log(findTopStudent(grades))
+
